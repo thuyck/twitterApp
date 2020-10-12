432e161bdfbccbdb055cf7169090ac92c0ac7890
Tristan Huyck <thuyck3@gmail.com>
2020-10-12 19:01:10 -0400

    App with features including log in, log out, pull to refresh, infinite scrolling, user stays logged in after closing app

---
+++ Twitter/tweetCell.swift
@@ -0,0 +1,29 @@
+//
+//  tweetCell.swift
+//  Twitter
+//
+//  Created by Tristan Alejandro Huyck on 10/11/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
